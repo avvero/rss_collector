f693d67f03a4df6b918e729abd0cba34b3cedec7
avvero <avvero@mail.ru>
2016-11-06 11:58:44 +0700

    camel

--- src/main/java/com/avvero/rss_collector/routes/CommonRoutes.java
+++ src/main/java/com/avvero/rss_collector/routes/CommonRoutes.java
@@ -3,6 +3,7 @@ package com.avvero.rss_collector.routes;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jackson.JacksonDataFormat;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,13 +15,16 @@ public class CommonRoutes extends RouteBuilder {
     @Autowired
     private JacksonDataFormat jsonFormat;
 
+    @Value("${rss_collector.queue_name}")
+    private String queueName;
+
     @Override
     public void configure() throws Exception {
         from("direct:push-event")
                 .setHeader("Content-Type", constant("application/json; charset=utf-8"))
                 .marshal(jsonFormat)
                 .log("${body}")
-                .to("activemq:rss");
+                .to("activemq:" + queueName);
     }
 
 }
